dbee2f398a69c01b86536be79e097cc4bd43d59e
Alexander Leontiev <aleontiev00@gmail.com>
2018-07-28 12:31:05 -0700

    Finished Directed graph. Finishing searching

--- Graph/DirectedGraph.cs
+++ Graph/DirectedGraph.cs
@@ -26,6 +26,7 @@ namespace Graph
             : base()
         {
             Vertices = new List<DVertex<T>>();
+            Edges = new List<Edge<T>>();
         }
         
         public override void AddVertex(DVertex<T> vertex)
@@ -40,7 +41,7 @@ namespace Graph
 
         public override bool AddEdge(DVertex<T> a, DVertex<T> b, int distance)
         {
-            if (GetEdge(a, b) == null)
+            if (GetEdge(a, b) != null)
             {
                 return false;
             }
@@ -63,12 +64,18 @@ namespace Graph
                 if (Edges[i].StartingPoint == vertex)
                 {
                     Edges.RemoveAt(i);
+                    i--;
                 }
                 else if (Edges[i].EndingPoint == vertex)
                 {
+                    Edges[i].StartingPoint.Neighbors.RemoveAt(Edges[i].StartingPoint.Neighbors.FindIndex((a) => { return a.EndingPoint == vertex; }));                    
                     Edges.RemoveAt(i);
+                    i--;
                 }
             }
+
+            Vertices.Remove(vertex);
+
             return true;
         }        
 
@@ -87,12 +94,12 @@ namespace Graph
 
         public override DVertex<T> Search(T value)
         {
-            throw new NotImplementedException();
+            return Vertices[Vertices.FindIndex((a) => { return a.Value.CompareTo(value) == 0; })];
         }
 
         public Edge<T> GetEdge(DVertex<T> a, DVertex<T> b)
-        {
-            if (!Vertices.Contains(a) && !Vertices.Contains(b))
+        {            
+            if (!(Vertices.Contains(a) && Vertices.Contains(b)))
             {
                 return null;
             }
@@ -106,6 +113,67 @@ namespace Graph
             }
 
             return null;
+        }
+
+        public void DepthFirstSearch(DVertex<T> start)
+        {
+            depthFirstSearch(start);
+
+            for (int i = 0; i < Count; i++)
+            {
+                this[i].IsVisited = false;
+            }
+
+            void depthFirstSearch(DVertex<T> current)
+            {
+                current.IsVisited = true;
+
+                foreach (var neighbor in current.Neighbors)
+                {
+                    if (!neighbor.EndingPoint.IsVisited)
+                    {
+                        Console.WriteLine($"\t{current.Value}->{neighbor.EndingPoint.Value}");
+                        depthFirstSearch(neighbor.EndingPoint);
+                    }
+                }
+
+                return;
+            }
+        }
+
+        //Fix this
+        public void BreadthFirstTraversal(DVertex<T> start)
+        {
+            breadthFirstTraversal(start, new Queue<DVertex<T>>(new DVertex<T>[] { start }));
+
+            for (int i = 0; i < Count; i++)
+            {
+                this[i].IsVisited = false;
+            }
+
+            void breadthFirstTraversal(DVertex<T> current, Queue<DVertex<T>> queue)
+            {
+                current.IsVisited = true;
+
+                foreach (var neighbor in current.Neighbors)
+                {
+                    if (!neighbor.IsVisite && !queue.Contains(neighbor.EndingPoint))
+                    {
+                        queue.Enqueue(neighbor.EndingPoint);
+                    }
+                }
+
+                queue.Dequeue();
+
+                if (queue.Count == 0)
+                {
+                    return;
+                }
+
+                Console.WriteLine($"\t{current.Value}->{queue.Peek().Value}");
+
+                breadthFirstTraversal(current, queue);
+            }
         }
     }
 }
--- Graph/Program.cs
+++ Graph/Program.cs
@@ -10,26 +10,51 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            UndirectedGraph<int> graph = new UndirectedGraph<int>();
+            UndirectedGraph<int> undirected = new UndirectedGraph<int>();
             
             for (int i = 0; i < 7; i++)
             {
-                graph.AddVertex(new Vertex<int>(i));
+                undirected.AddVertex(new Vertex<int>(i));
             }
 
-            graph.AddEdge(graph[0], graph[1]);
-            graph.AddEdge(graph[1], graph[2]);
-            graph.AddEdge(graph[1], graph[3]);
-            graph.AddEdge(graph[2], graph[3]);
-            graph.AddEdge(graph[2], graph[4]);
-            graph.AddEdge(graph[4], graph[6]);
-            graph.AddEdge(graph[6], graph[5]);               
+            undirected.AddEdge(undirected[0], undirected[1]);
+            undirected.AddEdge(undirected[1], undirected[2]);
+            undirected.AddEdge(undirected[1], undirected[3]);
+            undirected.AddEdge(undirected[2], undirected[3]);
+            undirected.AddEdge(undirected[2], undirected[4]);
+            undirected.AddEdge(undirected[4], undirected[6]);
+            undirected.AddEdge(undirected[6], undirected[5]);               
 
-            Console.WriteLine("Depth-First Search:");
-            graph.DepthFirstSearch(graph[0]);
+            Console.WriteLine("Undirected Depth-First Search:");
+            undirected.DepthFirstSearch(undirected[0]);
 
-            Console.WriteLine("\nBreadth-First Traversal:");
-            graph.BreadthFirstTraversal(graph[0]);                
+            Console.WriteLine("\nUndirected Breadth-First Traversal:");
+            undirected.BreadthFirstTraversal(undirected[0]);
+
+            DirectedGraph<int> directed = new DirectedGraph<int>();
+
+            directed.AddVertex(new DVertex<int>(0));//0
+            directed.AddVertex(new DVertex<int>(8));//1
+            directed.AddVertex(new DVertex<int>(5));//2
+            directed.AddVertex(new DVertex<int>(9));//3
+            directed.AddVertex(new DVertex<int>(7));//4
+
+            directed.AddEdge(directed[0], directed[1], 10); //0->8
+            directed.AddEdge(directed[0], directed[2], 5);  //0->5
+            directed.AddEdge(directed[1], directed[2], 2);  //8->5
+            directed.AddEdge(directed[1], directed[3], 1);  //8->9
+            directed.AddEdge(directed[2], directed[1], 3);  //5->8
+            directed.AddEdge(directed[2], directed[3], 9);  //5->9
+            directed.AddEdge(directed[2], directed[4], 2);  //5->7
+            directed.AddEdge(directed[3], directed[4], 4);  //9->7
+            directed.AddEdge(directed[4], directed[3], 6);  //7->9
+            directed.AddEdge(directed[4], directed[0], 7);  //7->0
+
+            Console.WriteLine("\nDirected Depth-First Search:");
+            directed.DepthFirstSearch(directed[0]);
+
+            Console.WriteLine("\nDirected Breadth-First Traversal:");
+            directed.BreadthFirstTraversal(directed[0]);
 
             Console.ReadKey();
         }        
